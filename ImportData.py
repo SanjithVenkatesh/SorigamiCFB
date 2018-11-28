a66d1dff86e4adbec85a4e02c6fad68d42741eb1
Sanjith Venkatesh <sanjith@dhcp-v000-147.mobile.uci.edu>
2018-11-28 12:42:18 -0800

    Fixed Import as well as created files till 2010

--- ImportData.py
+++ ImportData.py
@@ -5,6 +5,13 @@ from bs4 import BeautifulSoup as bs
 
 yearFiles = '/Users/sanjith/Documents/Projects/ScorigamiCFB/YearFiles/'
 
+def is_int(s):
+    try:
+        num = int(s)
+    except ValueError:
+        return False
+    return True
+
 def importData():
     for year in range(1869, 2011):
         if year != 1871:
@@ -15,13 +22,40 @@ def importData():
             print()
             dataRows = list()
             for line in dataLines:
-                stuff = line.split(' ')
-                fs = list(filter(lambda x: x != '', stuff))
-                dataRows.append(fs[0:-1])
-            print(dataRows)
+                if len(line) > 5:
+                    skip = False
+                    stuff = line.split(' ')
+                    fs = list(filter(lambda x: x != '', stuff))
+                    date = fs[0]
+                    homeTeamList = list()
+                    visitorTeamList = list()
+                    homeScore = 0
+                    visitorScore = 0
+                    count = 1
+                    while is_int(fs[count]) == False:
+                        homeTeamList.append(fs[count])
+                        count += 1
+                    homeScore = fs[count]
+                    count += 1
+                    print(fs, count)
+                    while count < len(fs) and is_int(fs[count]) == False :
+                        print(count)
+                        if fs[count] == "Institute136":
+                            visitorTeamList.append("Institute")
+                            visitorScore = 136
+                            skip = True
+                        else:
+                            visitorTeamList.append(fs[count])
+                        count += 1
+                    if skip == False:
+                        visitorScore = fs[count]
+                    homeTeam = " ".join(homeTeamList)
+                    visitorTeam = " ".join(visitorTeamList)
+                    fs = [date, homeTeam, int(homeScore), visitorTeam, int(visitorScore)]
+                    print(fs)
+                    dataRows.append(fs)
             yearDF = pd.DataFrame(dataRows, columns=("Date", "Home", "HomeScore", "Visitor", "VisitorScore"))[:-1]
-            print(yearDF)
-            yearDF.to_excel(yearFiles + str(year) + ".xlsx")
+            yearDF.to_excel(yearFiles + str(year) + ".xlsx", index = False)
 
 if __name__ == '__main__':
     importData()
